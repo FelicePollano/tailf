56db05f590266d803638e99e993b4b286606f9f2
felixpollan_cp <SND\felixpollan_cp>
2010-12-02 11:28:44 +0000

    First release

---
+++ Tailf/TestTailF/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net.Config;
+using log4net;
+using System.Threading;
+
+namespace TestTailF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure();
+            int i=0;
+            while (true)
+            {
+                var lg = LogManager.GetLogger(typeof(Program));
+                lg.Info("Message " + i++);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
